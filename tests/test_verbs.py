d80f52e48fef3adcd8223413d92c918d85328990
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2015-11-04 16:10:47 +0100

    tests: Add tests for OAI-PMH verb 'Identify'

--- tests/test_verbs.py
+++ tests/test_verbs.py
@@ -8,11 +8,115 @@
 # file for more details.
 
 from __future__ import absolute_import
+from unittest import TestCase
+from flask import request, Response, g
+from flask_oaiserver.oai import app
+from flask_oaiserver.config import *
+import re
 
-from helpers import FlaskTestCase
+
+class FlaskTestCase(TestCase):
+    """
+    Mix-in class for creating the Flask application
+    """
+
+    def setUp(self):
+        self.app = app
+        self.app.testing = True
+        pass
+
+    def tearDown(self):
+        #self.app = None
+        pass
 
 
 class TestVerbs(FlaskTestCase):
     """
     Tests OAI-PMH verbs
     """
+    def test_no_verb(self):
+        ########
+        ## TODO - remove all placeholder values
+        ########
+        with self.app.test_client() as c:
+            result = c.get('/oai2d', follow_redirects=True)
+            response_date = getattr(g, 'response_date', None)
+            expected = """<?xmlversion="1.0"encoding="UTF-8"?>
+<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
+         http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">
+    <responseDate>{0}</responseDate>
+    <error code="badValue">This is not a valid OAI-PMH verb:None</error>
+</OAI-PMH>""".format(response_date)
+        result_data = re.sub(' +','',result.data.replace('\n',''))
+        expected = re.sub(' +','',expected.replace('\n',''))
+        self.assertEqual(result_data, expected)
+
+    def test_wrong_verb(self):
+        ########
+        ## TODO - remove all placeholder values
+        ########
+        with self.app.test_client() as c:
+            result = c.get('/oai2d?verb=Aaa', follow_redirects=True)
+            response_date = getattr(g, 'response_date', None)
+            expected = """<?xmlversion="1.0"encoding="UTF-8"?>
+<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
+         http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">
+    <responseDate>{0}</responseDate>
+    <error code="badValue">This is not a valid OAI-PMH verb:Aaa</error>
+</OAI-PMH>""".format(response_date)
+        result_data = re.sub(' +','',result.data.replace('\n',''))
+        expected = re.sub(' +','',expected.replace('\n',''))
+        self.assertEqual(result_data, expected)
+
+    def test_identify(self):
+        ########
+        ## TODO - remove all placeholder values
+        ########
+        with self.app.test_client() as c:
+            result = c.get('/oai2d?verb=Identify', follow_redirects=True)
+            response_date = getattr(g, 'response_date', None)
+            expected = """<?xml version="1.0" encoding="UTF-8"?>
+<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
+         http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">
+    <responseDate>{0}</responseDate>
+    <request verb="Identify">http://memory.loc.gov/cgi-bin/oai</request>
+    <Identify>
+        <repositoryName>{1}</repositoryName>
+        <baseURL>http://memory.loc.gov/cgi-bin/oai</baseURL>
+        <protocolVersion>2.0</protocolVersion>
+        <adminEmail>{2}</adminEmail>
+        <earliestDatestamp>1990-02-01T12:00:00Z</earliestDatestamp>
+        <deletedRecord>transient</deletedRecord>
+        <granularity>YYYY-MM-DDThh:mm:ssZ</granularity>
+        <compression>deflate</compression>
+     </Identify>
+</OAI-PMH>""".format(response_date,CFG_SITE_NAME, CFG_ADMIN_EMAIL)
+        result_data = re.sub(' +','',result.data.replace('\n',''))
+        expected = re.sub(' +','',expected.replace('\n',''))
+        self.assertEqual(result_data, expected)
+
+    def test_identify_with_additional_args(self):
+        ########
+        ## TODO - remove all placeholder values
+        ########
+        with self.app.test_client() as c:
+            result = c.get('/oai2d?verb=Identify&notAValidArg=True', follow_redirects=True)
+            response_date = getattr(g, 'response_date', None)
+            expected = """<?xmlversion="1.0"encoding="UTF-8"?>
+<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
+         http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">
+    <responseDate>{0}</responseDate>
+    <request verb="Identify" notAValidArg="True">http://an.oa.org/OAI-script</request>
+    <error code="badArgument">You have passed too many arguments together with EXLUSIVE argument.</error>
+</OAI-PMH>""".format(response_date)
+        result_data = re.sub(' +','',result.data.replace('\n',''))
+        expected = re.sub(' +','',expected.replace('\n',''))
+        self.assertEqual(result_data, expected)
